0778a478f79bb25f7855c324c6c1473ccbd559eb
jongcoding <ialleejy@gmail.com>
2024-07-16 00:23:01 +0900

    UPDATE make index

--- WEAR_R_U/urls.py
+++ WEAR_R_U/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from WEAR_R_U.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", index),
 ]
 urlpatterns +=static (
     prefix=settings.MEDIA_URL,
